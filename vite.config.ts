36f1386c5b512370d75469d61c6c05c64699e471
Zcy <ourxzhang@qq.com>
2023-04-14 12:33:25 +0800

    feat: 代码格式化

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import * as path from 'path'
 import electron from 'vite-plugin-electron'
 // import electronRender from 'vite-plugin-electron-renderer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
